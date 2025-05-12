55573c4ec52e955de699f2ed784115e878e99bf3
Giovanni Battistella <battistg@tcd.ie>
2025-05-12 12:18:07 +0200

    Update sagre.py

--- sagre.py
+++ sagre.py
@@ -1,15 +1,18 @@
 import streamlit as st
 import datetime
 
-# Dati demo (aggiornati con la Sagra di San Isidoro di Schiavon)
+# Dati demo aggiornati con link GitHub (RAW)
+github_base_url = "https://github.com/battistg/sagra_advisor"
+
 sagre = {
     "2025-05-16": [
         {
             "nome": "Sagra di San Isidoro di Schiavon (VI)",
-            "menu_foto": ["/mnt/data/IMG_0273.jpeg"],
+            "menu_foto": [github_base_url + "IMG_0273.jpeg"],
             "piatti_foto": [
-                "/mnt/data/IMG_0266.jpeg",
-                "/mnt/data/IMG_0267.jpeg"
+                github_base_url + "IMG_0266.jpeg",
+                github_base_url + "IMG_0267.jpeg",
+                github_base_url + "IMG_0268.jpeg"
             ],
             "menu": {
                 "Gnocchi burro salvia": "6,00€",
@@ -17,14 +20,16 @@ sagre = {
                 "Gnocchi ragù d'anatra": "7,50€",
                 "Frittura mista di pesce con polenta e patatine fritte": "15,00€",
                 "Porzione patatine fritte": "3,50€",
-                "Grigliata mista": "13,00€",
+                "Grigliata mista (costine, salsiccia, galletto)": "13,00€",
                 "Costine e salsiccia (con patatine o fagioli)": "10,50€",
                 "Salsicce (con patatine o fagioli)": "9,00€",
                 "Galletto allo spiedo (con patatine o fagioli)": "9,00€",
-                "Pizze varie (es. Margherita, Patatosa, Salamin, ecc.)": "5,50-7,50€",
-                "Birre (33cl)": "3,50-4,00€",
-                "Caraffe vino": "6,00-9,00€",
-                "Spritz/Gin/Caffè": "1,30-3,00€"
+                "Pizze varie (Margherita, Patatosa, Salamin, Speck e Gorgonzola, ecc.)": "5,50-7,50€",
+                "Birra bionda/rossa (33cl)": "3,50-4,00€",
+                "Birra metro (10 birre)": "32,00-36,00€",
+                "Bicchiere di vino": "1,50€",
+                "Caraffa 1/2L o 1L vino": "6,00-9,00€",
+                "Spritz / Gin Tonic / Caffè": "1,30-3,00€"
             },
             "recensione": "Specialità frittura di pesce. La sagra è ben organizzata ma con alcuni alti e bassi tra pietanze, intrattenimento e location.",
             "voti": {
@@ -32,7 +37,7 @@ sagre = {
                 "Intrattenimento": "5.5/10 - Bene per i giovani, deludente per gli adulti, pista da ballo mal gestita",
                 "Pesca di Beneficenza": "9/10 - Ricca e con premi validi, ben sponsorizzata",
                 "Rabosello": "0/10 - Assente, grande delusione",
-                "Location": "7/10 - Ordinata e accogliente, mancano i cestini"
+                "Location": "7/10 - Ordinata e accogliente, ma mancano i cestini"
             }
         }
     ]
